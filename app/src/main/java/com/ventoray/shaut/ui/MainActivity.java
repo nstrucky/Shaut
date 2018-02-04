1c232f3e49def9259e6ab9560a4b690e0cc4b728
nstrucky <nickstruckmeyer@gmail.com>
2018-02-04 00:07:31 -0500

    Mocked up Places functionality: * replaced Picasso dependency with Glide for easy Bitmap use * added placeautocomletelistener to fragment * limit results to cities only * load city photo using sample code from api guide

--- app/src/main/java/com/ventoray/shaut/ui/MainActivity.java
+++ app/src/main/java/com/ventoray/shaut/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ventoray.shaut.ui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -11,9 +13,27 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlacePhotoMetadata;
+import com.google.android.gms.location.places.PlacePhotoMetadataBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadataResponse;
+import com.google.android.gms.location.places.PlacePhotoResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.ventoray.shaut.ui.util.MainActivityPagerAdapter;
 import com.ventoray.shaut.R;
 import com.ventoray.shaut.ui.util.FragmentPageTransformer;
@@ -36,8 +56,67 @@ public class MainActivity extends AppCompatActivity
         ButterKnife.bind(this);
         setUpNavDrawer();
         setUpViewPager();
+        initializePlaceAutoComplete();
     }
 
+
+    private void initializePlaceAutoComplete() {
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        AutocompleteFilter autocompleteFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_CITIES)
+                .build();
+        autocompleteFragment.setFilter(autocompleteFilter);
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                if (place == null) return;
+
+                getPlacePhoto(place.getId());
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.e("MainActivity", status.getStatusMessage());
+            }
+        });
+    }
+
+
+    private void getPlacePhoto(String placeId) {
+        final GeoDataClient geoDataClient = Places.getGeoDataClient(this, null);
+
+        Task<PlacePhotoMetadataResponse> photoMetadataResponseTask = geoDataClient
+                .getPlacePhotos(placeId);
+        photoMetadataResponseTask.addOnCompleteListener(new OnCompleteListener<PlacePhotoMetadataResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<PlacePhotoMetadataResponse> task) {
+                // Get the list of photos.
+                PlacePhotoMetadataResponse photos = task.getResult();
+                // Get the PlacePhotoMetadataBuffer (metadata for all of the photos).
+                PlacePhotoMetadataBuffer photoMetadataBuffer = photos.getPhotoMetadata();
+                // Get the first photo in the list.
+                PlacePhotoMetadata photoMetadata = photoMetadataBuffer.get(0);
+                // Get the attribution text.
+                CharSequence attribution = photoMetadata.getAttributions();
+                // Get a full-size bitmap for the photo.
+                Task<PlacePhotoResponse> photoResponse = geoDataClient.getPhoto(photoMetadata);
+                photoResponse.addOnCompleteListener(new OnCompleteListener<PlacePhotoResponse>() {
+                    @Override
+                    public void onComplete(@NonNull Task<PlacePhotoResponse> task) {
+                        PlacePhotoResponse photo = task.getResult();
+                        Bitmap bitmap = photo.getBitmap();
+
+                        Glide.with(MainActivity.this)
+                                .asBitmap()
+                                .load(bitmap)
+                                .into((ImageView) findViewById(R.id.imageView_city));
+                    }
+                });
+            }
+        });
+    }
 
     private void setUpNavDrawer() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
